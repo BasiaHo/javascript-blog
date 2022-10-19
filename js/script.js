6259fd76bb4c76a10d234b4f06a1d94017dfe8c6
Basia Hornuk <barbara.hornuk@outlook.com>
2022-10-19 20:10:01 +0200

    add function: remove class active from articles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 'use strict';
 
 function titleClickHandler(event){
+    const clickedElement = this;
   console.log('Link was clicked!', event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
+  for(let activeArticle of activeArticles){
+    activeLink.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
